a17c8f896c53266c76ad6236c74a74c849976d51
Thomas Flowers <flowersthomas434@gmail.com>
2024-02-26 22:17:00 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+#TODO create a basket
+#TODO create a login page
+#TODO create a default page
+#TODO allow user to filter the default page
+#TODO create an administrator that can add or remove
+#TODO allow the user to receive emails
+
